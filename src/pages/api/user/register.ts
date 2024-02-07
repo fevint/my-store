f85df47821d37960e5b490ca742afb13cf8bcbd1
fevint <fevint.k@gmail.com>
2024-02-07 14:45:59 +0700

    FORM REGISTER

---
+++ src/pages/api/user/register.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { signUp } from "@/lib/firebase/service";
+import { stat } from "fs";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    await signUp(req.body, (status: boolean) => {
+      if (status) {
+        res.status(200).json({ status: true, message: "Success" });
+      } else {
+        res.status(400).json({ status: false, message: "Gagal" }); 
+      }
+    });
+  } else {
+    res
+      .status(405)
+      .json({ status: false, statuscode: 405, message: "Method not allow" });
+  }
+}
